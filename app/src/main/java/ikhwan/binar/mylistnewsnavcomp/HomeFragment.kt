aff34316fa329431e9c039dd1949ed1bab135c89
Ikhwan Maulana Akbar <43781583+ikhwanma@users.noreply.github.com>
2022-06-06 23:11:51 +0700

    Remove warning

--- app/src/main/java/ikhwan/binar/mylistnewsnavcomp/HomeFragment.kt
+++ app/src/main/java/ikhwan/binar/mylistnewsnavcomp/HomeFragment.kt
@@ -3,14 +3,13 @@ package ikhwan.binar.mylistnewsnavcomp
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.os.bundleOf
-import androidx.navigation.NavOptions
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
--- app/src/main/java/ikhwan/binar/mylistnewsnavcomp/model/user/GetUserResponse.kt
+++
@@ -1,6 +0,0 @@
-package ikhwan.binar.mylistnewsnavcomp.model.user
-
-
-import com.google.gson.annotations.SerializedName
-
-class GetUserResponse : ArrayList<GetUserResponseItem>()
